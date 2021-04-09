10548d553b07303b978f35c9dee0f79683546bd0
jorbuedo_nexum <jordi.bueno@nexum.com>
2021-04-09 15:29:28 +0200

    Updates

---
+++ precache-manifest.ae58e21455836e76567735eaf522531e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed1965489a42c759040297b7afb942b",
+    "url": "/react-reactive-var/index.html"
+  },
+  {
+    "revision": "508e4e1de700473f090c",
+    "url": "/react-reactive-var/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "7df2fae513a6ba84c34b",
+    "url": "/react-reactive-var/static/js/2.6790a252.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-reactive-var/static/js/2.6790a252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508e4e1de700473f090c",
+    "url": "/react-reactive-var/static/js/main.dd9b734f.chunk.js"
+  },
+  {
+    "revision": "7ae661c815b6b6b13603",
+    "url": "/react-reactive-var/static/js/runtime-main.0b9619f0.js"
+  }
+]);
